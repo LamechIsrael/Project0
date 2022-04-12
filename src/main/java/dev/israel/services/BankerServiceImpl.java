6e8a8aa3d48431c0973d792ada9e55ef430ccb73
Lamech Israel <lamech639@revature.net>
2022-04-12 14:14:55 -0700

    Logger Application Commit

--- src/main/java/dev/israel/services/BankerServiceImpl.java
+++ src/main/java/dev/israel/services/BankerServiceImpl.java
@@ -2,6 +2,8 @@ package dev.israel.services;
 
 import dev.israel.data.BankerDAO;
 import dev.israel.entities.Banker;
+import dev.israel.utilities.BankerLogger;
+import dev.israel.utilities.BankerLoggerLevel;
 
 public class BankerServiceImpl implements BankerService{
 
@@ -14,24 +16,37 @@ public class BankerServiceImpl implements BankerService{
 
     @Override
     public Banker newBanker(Banker banker) {
+        BankerLogger.bankLogInfo("New banker " + banker.getFirstName() + " " + banker.getLastName() +
+                " added to Bank.", BankerLoggerLevel.INFO);
         return this.bankerDAO.createBanker(banker);
+
 
     }
 
     @Override
     public Banker loginBanker(String username, String password) {
         Banker banker = this.bankerDAO.loginBanker(username, password);
+        BankerLogger.bankLogInfo(banker.getFirstName() + " " + banker.getLastName() + " of username " + banker.getUsername() +
+                " has logged in.", BankerLoggerLevel.INFO);
         return banker;
     }
 
     @Override
     public Banker withdrawFunds(Banker banker, double amount) {
         if(amount>0){
+
+            // Withdrawl amount cannot be greater than the amount in the bank.
             if(banker.getBalance()>=amount) {
                 banker.setBalance(banker.getBalance() - amount);
                 this.bankerDAO.updateFunds(banker);
-            }else System.out.println("Not enough funds.");
+            }else if(banker.getBalance()<amount){
+                System.out.println("Not enough funds.");
+                BankerLogger.bankLogInfo(banker.getFirstName() + " " + banker.getLastName() + " of username " + banker.getUsername() +
+                        " tried to withdraw more funds than availale.", BankerLoggerLevel.ERROR);
+            }
         }else System.out.println("Not a valid amount.");
+        BankerLogger.bankLogInfo(banker.getFirstName() + " " + banker.getLastName() + " of username " + banker.getUsername() +
+                " has has withdrawn $" + amount + ".", BankerLoggerLevel.INFO);
 
         return banker;
     }
@@ -42,6 +57,9 @@ public class BankerServiceImpl implements BankerService{
             banker.setBalance(banker.getBalance() + amount);
             this.bankerDAO.updateFunds(banker);
         }else System.out.println("Not a valid amount.");
+
+        BankerLogger.bankLogInfo(banker.getFirstName() + " " + banker.getLastName() + " of username " + banker.getUsername() +
+                " has has deposited $" + amount + ".", BankerLoggerLevel.INFO);
 
 
         return banker;
